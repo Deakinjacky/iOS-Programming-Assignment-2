34e1e2387650e506e3998b9106a68dbef8610f54
Jacky <email.com>
2018-05-02 12:27:48 +1000

    Initial Commit
    Blank spritekit project

---
+++ SimpleArcadeGame/SimpleArcadeGame/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  SimpleArcadeGame
+//
+//  Created by Jacky Chen on 2/5/18.
+//  Copyright © 2018 Deakin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
